e83700eb958ef7c3a19a00c18a7c5b2d270f4b74
sim04ful <sim04ful@gmail.com>
2024-12-10 22:33:38 +0100

    first section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html"],
   theme: {
+    fontFamily: {
+      'sans': ['Space Mono', 'ui-sans-serif', 'system-ui',],
+      'serif': ['Noto Serif', 'ui-serif', 'Georgia',],
+    },
     extend: {},
   },
   plugins: [],
