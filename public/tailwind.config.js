c83a54ac6d8d9c4df79fc7ae7bdf69419aa03ed3
sim04ful <sim04ful@gmail.com>
2024-12-11 13:28:02 +0100

    added all assets

---
+++ public/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['Space Mono', 'ui-sans-serif', 'system-ui',],
+      'serif': ['Noto Serif', 'ui-serif', 'Georgia',],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
